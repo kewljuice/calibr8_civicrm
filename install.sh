812a579c24a3db590eeee61ead57b245a63f8216
wouter.hechtermans <wouter.hechtermans@calibrate.be>
2018-11-06 16:53:20 +0100

    refs #script removed lines.

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ $DOCKER_EXEC=$1
 $PROJECT_ROOT=$2
 
 # Add 'vcs' folders to composer.json.
-$DOCKER_EXEC composer config repositories.calibr8_civicrm vcs https://github.com/kewljuice/calibr8_civicrm
 $DOCKER_EXEC composer config repositories.civicrm-packages vcs https://github.com/civicrm/civicrm-packages
 $DOCKER_EXEC composer config repositories.zetacomponents-mail vcs https://github.com/civicrm/zetacomponents-mail.git
 $DOCKER_EXEC composer config repositories.civicrm-libraries vcs https://github.com/wannesderoy/civicrm-libraries
@@ -11,8 +10,7 @@ $DOCKER_EXEC composer config repositories.civicrm-drupal-8 vcs https://github.co
 $DOCKER_EXEC composer config repositories.civicrm-l10n vcs https://github.com/wannesderoy/civicrm-l10n
 
 # Add 'vcs' folders to composer.json.
-$DOCKER_EXEC require calibrate/calibr8_civicrm
-$DOCKER_EXEC run composer install
+$DOCKER_EXEC composer install
 
 # Copy folders to right directories.
 cp -rf $PROJECT_ROOT/data/www/vendor/civicrm/civicrm-packages $PROJECT_ROOT/data/www/vendor/civicrm/civicrm-core/packages/
